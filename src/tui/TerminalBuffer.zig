939b009ec0d8f3fb971873a306651831cdac2988
Russ <3870824+pocketfulofrage@users.noreply.github.com>
2026-04-22 12:51:58 -0400

    Update TerminalBuffer.zig

--- src/tui/TerminalBuffer.zig
+++ src/tui/TerminalBuffer.zig
@@ -1,5 +1,5 @@
 const std = @import("std");
-// const interop = @import("../interop.zig");
+const interop = @import("../interop.zig");
 const Cell = @import("Cell.zig");
 
 const Random = std.Random;
